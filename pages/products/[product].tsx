37d4990f1d903efbab4dfc3c6263a7f2e77ea87f
danielmahyar <thediamonds764@gmail.com>
2022-02-06 02:39:06 +0100

    Head test

--- pages/products/[product].tsx
+++ pages/products/[product].tsx
@@ -12,6 +12,31 @@ const ProductScreen = () => {
 		<>
 			<Head>
 				<title>{product}</title>
+				<meta name="description" content="Come and get the cheapest and most effective products" />
+
+				<meta property="og:type" content="website" />
+
+				<meta property="og:title" content={product.toString()} />
+
+				<meta property="og:description" content={"Specific item: " + product.toString()} />
+
+				<meta property="og:image" content="https://m.media-amazon.com/images/I/41tCIsGV8UL.jpg" />
+
+				<meta property="og:url" content="PERMALINK" />
+
+				<meta property="og:site_name" content="SITE NAME" />
+
+				<meta name="twitter:title" content="TITLE OF POST OR PAGE" />
+
+				<meta name="twitter:description" content="DESCRIPTION OF PAGE CONTENT" />
+
+				<meta name="twitter:image" content="LINK TO IMAGE" />
+
+				<meta name="twitter:site" content="@USERNAME" />
+
+				<meta name="twitter:creator" content="@USERNAME" />
+
+				<link rel="icon" href="favicon.ico" />
 			</Head>
 			<Header />
 			<main className="w-full h-full  space-y-8">
